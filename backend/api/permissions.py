ee757f3fb185d7b135040f4745ac5ed57b44c99f
Вячселав Наприенко <Hellon048@gmail.com>
2022-11-02 00:12:05 +0500

    review API fix

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from rest_framework.permissions import (IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
