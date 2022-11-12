6a446a6ce7bb8906ad8bb928f29195c6f105dd00
Вячселав Наприенко <Hellon048@gmail.com>
2022-11-13 02:13:59 +0500

    fix

---
+++ backend/api/filters.py
@@ -0,0 +1,44 @@
+from django_filters.rest_framework import FilterSet, filters
+
+from recipes.models import Ingredient, Recipe, Tag
+
+FILTER_USER = {'favorites': 'favorites__user',
+               'shop_list': 'shop_list__user'}
+
+
+class IngredientSearchFilter(FilterSet):
+    """Поиск по названию ингредиента."""
+    name = filters.CharFilter(lookup_expr='istartswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
+
+
+class RecipeFilter(FilterSet):
+    """Фильтр для рецептов."""
+
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+    )
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+
+    def _get_queryset(self, queryset, name, value, model):
+        if value:
+            return queryset.filter(**{FILTER_USER[model]: self.request.user})
+        return queryset
+
+    def filter_is_favorited(self, queryset, name, value):
+        return self._get_queryset(queryset, name, value, 'favorites')
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        return self._get_queryset(queryset, name, value, 'shop_list')
